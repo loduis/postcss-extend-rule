8ef32c4201eaf91add75b09031c29c810d3c3717
rasel-code-dev <raselmr005@gmail.com>
2021-11-26 20:21:16 +0600

    initial commit v3.0

--- src/index.js
+++ src/index.js
@@ -63,4 +63,4 @@ const plugin = (opts = {}) => {
 //   }
 // `);
 
-export default plugin;
+module.exports = plugin;
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 module.exports = {
-  mode: process.env.NODE_ENV !== "production" ? "development" : "production",
+  mode: process.env.NODE_ENV === "production" ? "development" : "production",
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
