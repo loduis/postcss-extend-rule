f0cc2d6daa480e046ae0ef4ac9fd3db9be56aece
Loduis Madariaga Barrios <loduis@gmail.com>
2022-11-03 08:27:18 -0500

    Update index.js
    Add remove funcional selector

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 const { name } = require("../package.json");
 
+// functional selector match
+const functionalSelectorMatch = /(^|[^\w-])(%[_a-zA-Z]+[_a-zA-Z0-9-]*)([^\w-]|$)/i;
+
 function getDeclarationsForSelector(selector, css) {
   const decls = [];
   css.walkRules(rule => {
     let ruleHasSelector = rule.selectors.some(ruleSelector => {
       return ruleSelector === selector;
     });
-    
+
     if (ruleHasSelector) {
       rule.walkDecls(decl => {
         decls.push(decl);
       });
     }
   });
-  
+
   return decls;
 }
 
@@ -25,7 +28,7 @@ function plugin(opts = {}) {
         // find all @applies classes...
         let classes = rule.params.toString().replace(/\s/g, " ").split(" ");
         classes.forEach(selector => {
-          
+
           // get all array of Declarations for @apply selector
           const decls = getDeclarationsForSelector(`${selector}`, rule.root());
           decls.forEach(decl => {
@@ -35,9 +38,14 @@ function plugin(opts = {}) {
             });
           });
         });
-        
+
         // remove all @apply rule
         rule.remove();
+      });
+      root.walkRules(functionalSelectorMatch, functionalRule => {
+        // manage encountered functional selectors
+        const functionalSelectorMessage = `Encountered functional selector "${functionalRule.selector}"`;
+        functionalRule.remove();
       });
     }
   };
