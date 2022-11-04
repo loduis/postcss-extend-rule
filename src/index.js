539cbf7e9a0fa1ba925bae7b87a892986c9af9fd
Loduis Madariaga Barrios <loduis@gmail.com>
2022-11-04 14:56:10 -0500

    Update index.js
    Fixed apply extends.

--- src/index.js
+++ src/index.js
@@ -1,50 +1,37 @@
-const { name } = require("../package.json");
+const { name } = require('../package.json');
 
 // functional selector match
 const functionalSelectorMatch = /(^|[^\w-])(%[_a-zA-Z]+[_a-zA-Z0-9-]*)([^\w-]|$)/i;
 
-function getDeclarationsForSelector(selector, css) {
-  const decls = [];
-  css.walkRules(rule => {
-    let ruleHasSelector = rule.selectors.some(ruleSelector => {
+function getExtendingRules(selector, root, postcss) {
+  const rules = [];
+  root.walkRules(rule => {
+    const ruleHasSelector = rule.selectors.some(ruleSelector => {
       return ruleSelector === selector;
     });
-
     if (ruleHasSelector) {
-      rule.walkDecls(decl => {
-        decls.push(decl);
-      });
+      rules.push(...rule.clone().nodes);
     }
   });
 
-  return decls;
+  return rules;
 }
 
 function plugin(opts = {}) {
   return {
     postcssPlugin: name,
-    Once(root) {
-      root.walkAtRules("extend", (rule) => {
-        // find all @applies classes...
-        let classes = rule.params.toString().replace(/\s/g, " ").split(" ");
-        classes.forEach(selector => {
-
-          // get all array of Declarations for @apply selector
-          const decls = getDeclarationsForSelector(`${selector}`, rule.root());
-          decls.forEach(decl => {
-            rule.parent.append({
-              prop: decl.prop,
-              value: decl.value
-            });
-          });
-        });
-
-        // remove all @apply rule
+    Once(root, { postcss }) {
+      root.walkAtRules('extend', (rule) => {
+        const selectors = rule.params.toString().replace(/\s/g, ' ').split(' ');
+        const root = rule.root();
+        const parent = rule.parent;
+        for (const selector of selectors) {
+          parent.append(...getExtendingRules(selector, root));
+        }
         rule.remove();
       });
+
       root.walkRules(functionalSelectorMatch, functionalRule => {
-        // manage encountered functional selectors
-        const functionalSelectorMessage = `Encountered functional selector "${functionalRule.selector}"`;
         functionalRule.remove();
       });
     }
